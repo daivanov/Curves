e7a11a756621fa628a8d8a871c0e880f986a7e9f
Daniil Ivanov <daniil.ivanov@gmail.com>
2014-01-28 19:29:24 +0200

    Skip short segments also in first and second derivatives

--- tools/visualize.m
+++ tools/visualize.m
@@ -1,20 +1,25 @@
 close all; clear all;
 
-function P2 = skip_short(P)
+function P2 = skip_short(P, tolerance)
   L = diff(P);
   L = sqrt(L(:,1).^2 + L(:,2).^2);
   P2 = P;
-  P2(find(L < 2) + 1, :) = [];
+  P2(find(L < tolerance) + 1, :) = [];
+end
+
+function L = distances(P)
+  Q2 = diff(P);
+  L = sqrt(Q2(:,1).^2 + Q2(:,2).^2);
 end
 
 function A = angles(P)
   Q2 = diff(P);
   A = Q2(:,2) ./ sqrt(Q2(:,1).^2 + Q2(:,2).^2);
   A = asin(A);
-  z = (Q2(:,1) < 0);
+  z = find(Q2(:,1) < 0);
   A(z) = pi - A(z);
   T = diff(A);
-  z = (abs(T) > pi);
+  z = find(abs(T) > pi);
   T(z) = T(z) - 2 * pi * sign(T(z));
   A = cumsum(T);
   A = A / pi * 180;
@@ -35,16 +40,17 @@ T = D(:,3);
 % length 3 / sigma 6
 kernel = [0.3317865678454581, 0.33642686430908364, 0.3317865678454581];
 
-MC = skip_short(M);
+M = skip_short(M, 2);
+MC = M;
 MC(:,1) = convpadded(MC(:,1), kernel, 'same');
 MC(:,2) = convpadded(MC(:,2), kernel, 'same');
 
-M1 = diff(MC, 1);
+M1 = skip_short(diff(MC, 1), 0.1);
 M1C = M1;
 M1C(:,1) = convpadded(M1(:,1), kernel, 'same');
 M1C(:,2) = convpadded(M1(:,2), kernel, 'same');
 
-M2 = diff(M1C, 1);
+M2 = skip_short(diff(M1C, 1), 0.1);
 M2C = M2;
 M2C(:,1) = convpadded(M2(:,1), kernel, 'same');
 M2C(:,2) = convpadded(M2(:,2), kernel, 'same');
